a439e02f4b903004957757e898956522a4025ca7
Alan Fransham <alataffective@outlook.com>
2019-05-21 11:46:13 +0100

    Added project files

---
+++ XamarinExample.macOS/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+using AppKit;
+using Foundation;
+
+namespace XamarinExample.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow window;
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "Xamarin Example";
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow
+        {
+            get { return window; }
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+    }
+}
---
+++ XamarinExample/XamarinExample/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace XamarinExample
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            BindingContext = new ViewModel();
+        }
+    }
+
+    class ViewModel
+    {
+        readonly Thing[] _things;
+
+        public ViewModel()
+        {
+            _things = new Thing[]
+            {
+                new Thing("A thing"),
+                new Thing("B thing"),
+                new Thing("C thing")
+            };
+        }
+
+        public IEnumerable<Thing> Things => _things;
+    }
+
+    class Thing
+    {
+        public Thing(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
